f2719ec928039e2fd64ea250a6782afcdb4d28e0
25th night <jinjapan97@gmail.com>
2026-02-19 23:15:35 +0900

    fix lint

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -21,95 +21,95 @@ const (
 //
 //	we received a heartbeat from it.
 type Node struct {
-  Addr string
-  CurrStatus NodeStatus
-  LastHB time.Time
+	Addr       string
+	CurrStatus NodeStatus
+	LastHB     time.Time
 }
 
 // Registry keeps track of all known nodes and their health.
 //
 //	a timeout after which a node is considered dead.
 type Registry struct {
-  AddrNode map[string]Node
-  mu sync.Mutex
-  timeOut time.Duration
+	AddrNode map[string]Node
+	mu       sync.Mutex
+	timeOut  time.Duration
 }
 
 // NewRegistry creates a Registry that marks nodes dead after the given timeout.
 func NewRegistry(healthTimeout time.Duration) *Registry {
-  r := &Registry{
-    AddrNode: make(map[string]Node),
-    timeOut: healthTimeout,
-  }
+	r := &Registry{
+		AddrNode: make(map[string]Node),
+		timeOut:  healthTimeout,
+	}
 
-  ticker := time.NewTicker(healthTimeout)
-  go func() {
-    for {
-      select {
-        case <- ticker.C:
-          r.checkHealth()
-      }
-    }
-  }()
-  return r
+	ticker := time.NewTicker(healthTimeout)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				r.checkHealth()
+			}
+		}
+	}()
+	return r
 }
 
 // Register adds a new node to the cluster or updates an existing one's heartbeat.
 func (r *Registry) Register(addr string) {
-  r.mu.Lock()
-  defer r.mu.Unlock()
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
-  node, ok := r.AddrNode[addr]
-  if !ok {
-    r.AddrNode[addr] = Node{
-      Addr: addr,
-      CurrStatus: StatusAlive,
-      LastHB: time.Now(),
-    }
-  } else {
-    node.LastHB = time.Now()
-    r.AddrNode[addr] = node
-  }
+	node, ok := r.AddrNode[addr]
+	if !ok {
+		r.AddrNode[addr] = Node{
+			Addr:       addr,
+			CurrStatus: StatusAlive,
+			LastHB:     time.Now(),
+		}
+	} else {
+		node.LastHB = time.Now()
+		r.AddrNode[addr] = node
+	}
 }
 
 // Heartbeat updates the last-seen time for a node.
 func (r *Registry) Heartbeat(addr string) {
-  r.mu.Lock()
-  defer r.mu.Unlock()
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
-  node, ok := r.AddrNode[addr]
-  if !ok {
-    return
-  }
-  
-  node.LastHB = time.Now()
-  if node.CurrStatus == StatusSuspect {
-    node.CurrStatus = StatusAlive
-  }
+	node, ok := r.AddrNode[addr]
+	if !ok {
+		return
+	}
 
-  r.AddrNode[addr] = node
+	node.LastHB = time.Now()
+	if node.CurrStatus == StatusSuspect {
+		node.CurrStatus = StatusAlive
+	}
+
+	r.AddrNode[addr] = node
 }
 
 // Unregister removes a node from the cluster.
 func (r *Registry) Unregister(addr string) {
-  r.mu.Lock()
-  defer r.mu.Unlock()
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
-  delete(r.AddrNode, addr)
+	delete(r.AddrNode, addr)
 }
 
 // AliveNodes returns the addresses of all nodes currently StatusAlive.
 func (r *Registry) AliveNodes() []string {
-  r.mu.Lock()
-  defer r.mu.Unlock()
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
-  addr := []string{}
-  for add, node := range r.AddrNode {
-    if node.CurrStatus == StatusAlive {
-      addr = append(addr, add)
-    }
-  }
-  return addr
+	addr := []string{}
+	for add, node := range r.AddrNode {
+		if node.CurrStatus == StatusAlive {
+			addr = append(addr, add)
+		}
+	}
+	return addr
 }
 
 // checkHealth iterates over all nodes and marks those with stale heartbeats
@@ -117,17 +117,17 @@ func (r *Registry) AliveNodes() []string {
 //
 //	If last heartbeat > 2*timeout, mark StatusDead and remove.
 func (r *Registry) checkHealth() {
-  r.mu.Lock()
-  defer r.mu.Unlock()
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
-  for add, node := range r.AddrNode {
-    if time.Since(node.LastHB) > r.timeOut {
-      node.CurrStatus = StatusSuspect
-      r.AddrNode[add] = node
-    } 
-    if time.Since(node.LastHB) > 2*r.timeOut {
-      node.CurrStatus = StatusDead
-      delete(r.AddrNode, add)
-    }
-  }
+	for add, node := range r.AddrNode {
+		if time.Since(node.LastHB) > r.timeOut {
+			node.CurrStatus = StatusSuspect
+			r.AddrNode[add] = node
+		}
+		if time.Since(node.LastHB) > 2*r.timeOut {
+			node.CurrStatus = StatusDead
+			delete(r.AddrNode, add)
+		}
+	}
 }
