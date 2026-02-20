c533bf3475751cfa3efd911c5df72ea266e81a8a
25th night <jinjapan97@gmail.com>
2026-02-20 22:29:10 +0900

    finished client.go

--- client/client.go
+++ client/client.go
@@ -1,63 +1,131 @@
 package client
 
-import "time"
+import (
+	"encoding/json"
+	"net"
+	"time"
+
+	"github.com/BiChong-Jin/distributed-cache/protocol"
+)
 
 // -------- Client SDK --------
 // This is what application code uses to talk to the cache cluster.
 // The client connects to ANY node; that node routes the request to the right place.
 
 // Client is a cache client that connects to a cluster node.
-// TODO: Store the server address and optionally a connection pool.
 type Client struct {
-	// YOUR CODE HERE
+	Addr string
 }
 
 // NewClient creates a client that talks to the cache cluster via the given node address.
 func NewClient(addr string) *Client {
-	// YOUR CODE HERE
-	return nil
+	return &Client{Addr: addr}
 }
 
 // Close cleans up any open connections.
 func (c *Client) Close() error {
-	// YOUR CODE HERE
 	return nil
 }
 
 // Set stores a key-value pair with the given TTL.
-// TODO: Build a protocol.Request with CmdSet, encode it, send over TCP, read response.
 func (c *Client) Set(key string, value []byte, ttl time.Duration) error {
-	// YOUR CODE HERE
+	req := &protocol.Request{
+		CommandType: protocol.CmdSet,
+		Key:         key,
+		Value:       value,
+		TTL:         ttl,
+	}
+
+	_, err := c.sendRequest(req)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // Get retrieves a value by key.
-// TODO: Build a protocol.Request with CmdGet, send it, decode response.
 func (c *Client) Get(key string) ([]byte, error) {
-	// YOUR CODE HERE
-	return nil, nil
+	req := &protocol.Request{
+		CommandType: protocol.CmdGet,
+		Key:         key,
+	}
+
+	resp, err := c.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Value, nil
 }
 
 // Delete removes a key.
 func (c *Client) Delete(key string) error {
-	// YOUR CODE HERE
+	req := &protocol.Request{
+		CommandType: protocol.CmdDelete,
+		Key:         key,
+	}
+
+	_, err := c.sendRequest(req)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // Keys returns all keys in the cluster (queries the connected node).
 func (c *Client) Keys() ([]string, error) {
-	// YOUR CODE HERE
-	return nil, nil
+	req := &protocol.Request{
+		CommandType: protocol.CmdKeys,
+	}
+
+	resp, err := c.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var keys []string
+	err = json.Unmarshal(resp.Value, &keys)
+	if err != nil {
+		return nil, err
+	}
+
+	return keys, nil
 }
 
 // Ping checks whether the connected node is alive.
 func (c *Client) Ping() error {
-	// YOUR CODE HERE
+	req := &protocol.Request{
+		CommandType: protocol.CmdPing,
+	}
+
+	_, err := c.sendRequest(req)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // sendRequest is a helper that handles the TCP send/receive cycle.
-// TODO: Dial (or reuse connection), write encoded request, read and decode response.
-func (c *Client) sendRequest( /* *protocol.Request */ ) /* (*protocol.Response, error) */ {
-	// YOUR CODE HERE
+func (c *Client) sendRequest(req *protocol.Request) (*protocol.Response, error) {
+	conn, err := net.Dial("tcp", c.Addr)
+	if err != nil {
+		return &protocol.Response{StatusCode: protocol.StatusError, ErrorMessage: "Cannot connect to the cluster."}, err
+	}
+	defer conn.Close()
+
+	reqBytes, err := req.Encode()
+	if err != nil {
+		return &protocol.Response{StatusCode: protocol.StatusError, ErrorMessage: "Bad request."}, err
+	}
+
+	conn.Write(reqBytes)
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return &protocol.Response{StatusCode: protocol.StatusError, ErrorMessage: "Cannot get response."}, err
+	}
+	resp := buf[:n]
+
+	return protocol.DecodeResponse(resp)
 }
